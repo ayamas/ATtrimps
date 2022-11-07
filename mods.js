c07238a6629f998ae219357a594c61a64236003a
ayamas <mr_roccox@hotmail.com>
2022-11-07 10:02:24 +0800

    fix

--- mods.js
+++ mods.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Ray';
-script.src = 'https://Psycho-Ray.github.io/AutoTrimps/AutoTrimps2.js';
+script.id = 'AutoTrimps-Ayamas';
+script.src = 'https://ayamas.github.io/ATtrimps/AutoTrimps2.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
--- modsGRAPH.js
+++ modsGRAPH.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Graphs';
-script.src = 'https://Psycho-Ray.github.io/AutoTrimps/GraphsOnly.js';
+script.src = 'https://ayamas.github.io/ATtrimps/GraphsOnly.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);

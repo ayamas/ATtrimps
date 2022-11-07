2d215ceea241840e14aaab42e6df5a3fce1564d0
ayamas <mr_roccox@hotmail.com>
2022-11-07 10:11:38 +0800

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -140,7 +140,6 @@ function mainLoop() {
     if (ATrunning == false) return;
     if (getPageSetting('PauseScript') || game.options.menu.pauseGame.enabled || game.global.viewingUpgrades) return;
     ATrunning = true;
-	debug("SET SPEED!!!");
     if (getPageSetting('showbreedtimer') == true) {
         if (game.options.menu.showFullBreed.enabled != 1) toggleSetting("showFullBreed");
         addbreedTimerInsideText.innerHTML = ((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) + 's'; //add breed time for next army;
@@ -175,6 +174,7 @@ function mainLoop() {
         }
 
         //Core
+		debug("SET SPEED!!!");
         if (getPageSetting('AutoMaps') > 0) {
             autoMap(hdStats, vmStatus);
         } else if (getPageSetting('showautomapstatus')) {
